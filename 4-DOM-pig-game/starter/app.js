3bd871f0f7022f0f776c42070589f13a0ba3fb86
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2018-12-16 18:07:36 -0500

    Added in a dice function to get random dice values.  Also using querySelector to read and change content, HTML, and CSS.

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,39 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+dice = Math.floor(Math.random() * 6) +1;
+
+console.log(dice);
+
+/*The document object refers to the DOM that is generated from the HTML.
+The .querySelector method allows us to specify an HTML element (in this case,
+we're getting the id of #score-0) and we're adding the result of activePlayer
+because all player-specific elements have the same names, except for the number.
+
+
+the .textContent method allows me to
+change what is inside the tags.  
+*/
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+/* Goal of this is to make the dice invisible.  Use document.querySelector(
+.dice) because the die only has a class, no id.  Then .style says we are going
+to change the CSS, the next element (.display) tells us which property we are
+going to change, and then what follows the = sign is what we are setting that
+property to - in this case, 'none' because we want it hidden.  remember that
+the property has to be a string.
+
+*/
+
+document.querySelector('.dice').style.display = "none";
