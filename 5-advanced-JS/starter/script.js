d82d76fd30d61b493ec8a894575d13aa3f9b9f46
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2018-12-23 18:09:27 -0500

    5.64 Some exploration of how variables store primitives and objects.

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,5 @@
+/* 
 
-/**
 // function constructor
 
 var john = {
@@ -37,10 +37,6 @@ mark.calculateAge();
 console.log(mark.species);
 console.log(mark);
 
-
-
- */
-
  ////  5.63  Object.create  ////////
 
  var personProto = {
@@ -49,5 +45,72 @@ console.log(mark);
      }
  };
 
+
+ // If you pass .create() an argument, it uses that as the prototype.
  var john = Object.create(personProto);
 
+ john.name = 'John';
+ john.yearOfBirth = 1990;
+ john.job = 'teacher';
+
+// It's kind of like the reverse of the function constructor.  Here,
+// the prototype is easy to specify and the functions are passed in.  
+ 
+var jane = Object.create(personProto,{
+    name: { value: 'Jane' },
+    yearOfBirth: { value:1969 },
+    job: {value: "Nurse"},
+
+});
+
+*/
+
+//// Section 5.64 Primitives vs. objects
+
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a,b);
+
+//returns two different values because variable actually hold the values
+//of primitives.  Mutating  a  does not affect  b  because  b  is actually
+// holding its own value.
+
+var obj1 = {
+    name: 'John',
+    age: 26,
+
+}
+
+var obj2 = obj1;
+
+obj1.job = 'teacher';
+
+console.log(obj1.job, obj2.job)
+
+// this is not the case for objects.  obj2 does not contain a separate
+// instance of obj1.  They both point at the same place so when one
+// changes, both change.  
+
+var age = 27;
+
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon',
+};
+
+
+// b.city will change because it's just a pointer to the .city property of obj.
+// However, age remains unchanged.  We just created a new variable called a
+// whose value is never read.  To change the orginal, we would have to say
+// age = 30;
+
+function change(a,b){
+    a = 30;
+    b.city = "San Francisco";
+}
+
+change(age, obj);
+
+console.log(age, obj.city);
