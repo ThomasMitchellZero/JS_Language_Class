bb6eded456155b941c889ebba1a1e69fd41921ef
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2018-12-27 21:30:23 -0500

    6.82 - Budgety project.  Added in an addItem method to Budget Controller that creates a unique ID for each entry, and then assigns it to the correct place in the database.   Everything 's currently working, but needs a lot of commenting to explain what is going on.  Manana.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -33,8 +33,64 @@ UPDATE THE UI
 
 // BUDGET CONTROLLER ////
 var budgetController = (function(){
-    
+
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    // not sure why this is the best data structure.  Hoping for clarification later today.
+    var data = {
+        allItems: {
+            exp:[],
+            inc:[],
+        },
+        totals: {
+            exp: 0,
+            inc: 0,
+        },
+    };
+
+    return {
+
+        // This function adds the user's inputs to the database.
+        addItem: function(type, des, val){
+            
+            var newItem, ID;
+                // type will be either inc or exp, so this picks the appropriate array to check. This gets the lenght of the chosen array and then this just gets the id property(the number) of the last item in the array, then adds 1 to that.  This generates a new ID 1 bigger than the previous largest.
+
+            if(data.allItems[type].length > 0){
+                ID = (data.allItems[type][data.allItems[type].length -1].id +1);
+            } else {
+                ID = 0;
+            };
+            
+            
+            
+            // Makes a new Income or Expense object based on the type the user inputs.
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+
+            } else {
+                newItem = new Income(ID, des, val);
+
+            };
+            // I don't remember seeing this pattern before.  Looks like the brackets allow you to specify the property you want to access by passing a string to the brackets?
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        testing: function(){console.log(data)},
+    };
 })();
+
 
 // UI CONTROLLER ////
 
@@ -73,16 +129,35 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER ////
 var controller = (function(budgetCtrl, UIctrl){
 
-    // we import the DOMstrings object from UIcontrol moduel that we made available via 
-    var DOM = UIctrl.getDOMstrings();
+    var setupEventListeners = function(){
+        // DOM elements are only needed for event listeners, so they can go in here.  We're importing the contents of the DOMstring object from the UIcontrol module.
+
+        var DOM = UIctrl.getDOMstrings();
+
+        // listens for a click on .add__btn and runs the ctrlAddItem function.
+        document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
+
+        // this is how we listen for a key being pressed - in this case, Enter
+        document.addEventListener("keypress", function(event){
+            if (event.keyCode === 13){
+                ctrlAddItem();
+            }
+        });
+
+    };
+    setupEventListeners();
+
+
     // Does all necessary operations for when the user hits Enter.
     var ctrlAddItem = function(){
 
-    // get the field input data
+        var input, newItem;
         
-        // UIctrl points to UIcontroller because we pass it as a variable to the controller function, and getInput is a method of UIController.
+        // This stores the values that the user enters as an object.
         var input = UIctrl.getInput();
-        console.log(input);
+
+        // takes the object in  input  and passes its properties as arguments to the addItem method from budgetController.  Then it stores the entry as an object in the database.
+        newItem = budgetCtrl.addItem (input.type, input.description, input.value);
 
 
 
@@ -93,16 +168,18 @@ var controller = (function(budgetCtrl, UIctrl){
         // Calculate the budget
 
         // Display the budget in the UI
+    };
+
+    // ONLY returned objects will EVER be accessible outside the scope of an IIFE.
+    return {
+        // again, not sure why we have the anonymous function insteaod of just calling setupEventListeners();
+        init: function(){
+            console.log('app has started');
+            setupEventListeners();
+
+        },
     }
 
-    // listens for a click on .add__btn and runs the ctrlAddItem function.
-    document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
-
-    // this is how we listen for a key being pressed - in this case, Enter
-    document.addEventListener("keypress", function(event){
-        if (event.keyCode === 13){
-            ctrlAddItem();
-        }
-    })
-
 })(budgetController, UIController)
+
+controller.init();
