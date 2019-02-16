2d2f308545ac69e5e17ec9ea3e8a207edfbb05fc
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2019-02-16 10:38:03 -0500

    8.127 - changed slightly to work with asyn/await

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -11,3 +11,31 @@ function getWeather(woeid){
 }
 getWeather(2487956);
 getWeather(44418);
+
+// Here's the version with async/await
+async function getWeatherAW(woeid){
+
+    // the code will try to execute everything in curly brackets, but if there is an errory, the catch() function will be run instead.
+    try {
+        const result = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+
+        const data = await result.json()
+        const tomorrow = data.consolidated_weather[1];
+
+        console.log(`Temperatures in ${data.title} tomorrow will be between ${Math.round(tomorrow.min_temp)} and ${Math.round(tomorrow.max_temp)} degrees C`);
+
+        return data;
+    } catch(error){
+        console.log(error);
+    }
+    
+}
+getWeatherAW(2487956);
+
+
+let dataLondon;
+getWeatherAW(44418).then((data) => {
+    dataLondon = data
+    console.log(dataLondon);
+});
+
